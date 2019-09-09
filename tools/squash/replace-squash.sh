72f0e75795f4c3e36622628c3c4dae0d608eb19c
igrek51 <igrek.s@o2.pl>
2019-09-10 00:21:02 +0200

    Add script replacing squash filesystem on the run

--- tools/squash/replace-squash.sh
+++ tools/squash/replace-squash.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
-export SOURCE_SQUASH=/media/user/data/ext/watchmaker/squash/filesystem.squashfs
+set -ex
+
+export SQUASH_STORAGE_PATH=/media/user/data/ext/watchmaker/squash
+export SQUASH_FS_STORAGE_PATH=$SQUASH_STORAGE_PATH/filesystem.squashfs
+export TAGGED_SQUASH_PATH=$SQUASH_STORAGE_PATH/filesystem-${DATE}.squashfs
 export TARGET_SQUASH=/media/user/live/live/filesystem.squashfs
+DATE=`date +%Y-%m-%d`
 
 # root only
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    exit 1
 fi
 
-echo "source checksum:"
-cksum $SOURCE_SQUASH
+# ensure mount points are mounted
+test -d $SQUASH_STORAGE_PATH
+test -f $TARGET_SQUASH
 
-read -p "[WARNING] Attempting to replace squash from $SOURCE_SQUASH to $TARGET_SQUASH. Are you sure?"
+cd /
+
+rm -f $SQUASH_FS_STORAGE_PATH
+sync
+
+echo "squashing filesystem..."
+mksquashfs \
+	/bin /boot /dev /etc /home /lib /lib64 /media /mnt /opt /proc /run /root /sbin /srv /sys /tmp /usr /var \
+	/initrd.img /initrd.img.old /vmlinuz /vmlinuz.old \
+	$SQUASH_FS_STORAGE_PATH \
+	-regex -ef $SQUASH_STORAGE_PATH/EXCLUDE_FILE \
+	-comp gzip -b 512k \
+	-keep-as-directory
+
+echo "creating tagged copy: $TAGGED_SQUASH_PATH..."
+cp $SQUASH_FS_STORAGE_PATH $TAGGED_SQUASH_PATH
+sync
 
 echo "removing old squashfs: $TARGET_SQUASH"
 rm -f $TARGET_SQUASH
 
 echo "rsync"
-rsync -ah --progress $SOURCE_SQUASH $TARGET_SQUASH
-
+rsync -ah --progress $SQUASH_FS_STORAGE_PATH $TARGET_SQUASH
 echo "Syncing..."
 sync
 
 echo "source checksum:"
-cksum $SOURCE_SQUASH
+cksum $SQUASH_FS_STORAGE_PATH
 echo "target checksum:"
 cksum $TARGET_SQUASH
 
