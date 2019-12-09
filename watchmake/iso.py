572e87f69465c6c392bd0d443438c80547a62af1
igrek51 <igrek51.dev@gmail.com>
2019-12-09 23:44:36 +0100

    Add creating ISO from non-empty disk partitions

--- watchmake/iso.py
+++ watchmake/iso.py
@@ -9,26 +9,26 @@ from system import wrap_shell, confirm
 
 def make_iso(yes: bool, source_disk: str, target_iso: str):
     info(f'checking required files existence')
-    assert os.path.exists(os.path.join(target_iso, os.pardir))
+    assert os.path.exists(os.path.abspath(os.path.join(target_iso, os.pardir)))
 
     fdisk_output = shell_output(f'sudo fdisk -l {source_disk}')
     print(fdisk_output)
 
     block_size_line = [line for line in fdisk_output.splitlines() if line.startswith('Units: sectors of')][0]
     block_size_matcher = re.compile(r'= ([0-9]+) bytes$')
-    match = block_size_matcher.match(block_size_line)
+    match = block_size_matcher.search(block_size_line)
     assert match
     block_size = int(match.group(1))
 
     end_sector_line = fdisk_output.splitlines()[-1]
     end_sector_matcher = re.compile(f'^{source_disk}[a-z0-9]*\\s+[0-9]+\\s+([0-9]+)\\s+[0-9]+')
-    match = end_sector_matcher.match(end_sector_line)
+    match = end_sector_matcher.search(end_sector_line)
     assert match
     end_sector = int(match.group(1))
 
     info(f'block size: {block_size}')
     info(f'end sector: {end_sector}')
-    confirm(yes, f'Attempting dump partitions from {source_disk} to {target_iso}. Are you sure?')
+    confirm(yes, f'Attempting to dump partitions from {source_disk} to {target_iso}. Are you sure?')
 
     info(f'Writing {source_disk} to {target_iso}')
     wrap_shell(
